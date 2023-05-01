7c767998114dd0c4632198f6612f446b7894b6e3
Astavie <astavie@pm.me>
2023-05-02 00:26:28 +0200

    feat: nix support
    * nix support
    
    * update flake
    
    * reset main.rs
    
    * update version

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
+
+  inputs.fenix.url = github:nix-community/fenix;
+  inputs.fenix.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs = { self, nixpkgs, fenix }:
+    let
+      name = "flatland";
+      pkgs = system: import nixpkgs {
+        inherit system;
+      };
+      shell = pkgs: pkgs.mkShell {
+        inputsFrom = [ self.packages.${pkgs.system}.default ];
+      };
+      package = pkgs:
+        let
+          toolchain = fenix.packages.${pkgs.system}.minimal.toolchain;
+        in
+          (pkgs.makeRustPlatform {
+            cargo = toolchain;
+            rustc = toolchain;
+          }).buildRustPackage rec {
+            pname = name;
+            src = ./.;
+
+            # ---- START package specific settings ----
+            version = "0.8.0";
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+
+            STARDUST_RES_PREFIXES = ./res;
+
+            buildInputs = with pkgs; [
+              libxkbcommon xorg.libxcb
+            ];
+            # ---- END package specific settings ----
+          };
+    in
+    {
+      overlays.default = final: prev: {
+        stardust-xr.${name} = package final;
+      };
+
+      packages."x86_64-linux".default = package (pkgs "x86_64-linux");
+      packages."aarch64-linux".default = package (pkgs "aarch64-linux");
+
+      devShells."x86_64-linux".default = shell (pkgs "x86_64-linux");
+      devShells."aarch64-linux".default = shell (pkgs "aarch64-linux");
+    };
+}
